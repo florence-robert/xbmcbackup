8b759f0ef13b9bae4ba91e1002b14f1c4ac1c862
Frank Robert <frankie.robert+github@gmail.com>
2023-04-25 11:47:02 -0400

    🎉 feat(default.py): add support for command line arguments and refactor code This commit adds support for command line arguments to the script. It also refactors the code to use an enum to represent the different modes of operation. The commit also adds utility functions to handle authorization of cloud providers, removing authorization, and extracting parameters from the command line arguments. The commit also adds string constants to improve readability and maintainability of the code. Finally, the commit adds functions to handle the different modes of operation, including backup, restore, settings, advanced editor, and launcher.

--- default.py
+++ default.py
@@ -1,145 +1,198 @@
+import sys
+import xbmc
 import xbmcgui
 import xbmcvfs
 import resources.lib.utils as utils
 from resources.lib.backup import XbmcBackup
 from resources.lib.authorizers import DropboxAuthorizer
 from resources.lib.advanced_editor import AdvancedBackupEditor
+from urllib.parse import urlparse, parse_qs
+from enum import Enum, auto
 
-# mode constants
-BACKUP = 0
-RESTORE = 1
-SETTINGS = 2
-ADVANCED_EDITOR = 3
-LAUNCHER = 4
+# Mode constants
+class Mode(Enum):
+    BACKUP = auto()
+    RESTORE = auto()
+    SETTINGS = auto()
+    ADVANCED_EDITOR = auto()
+    LAUNCHER = auto()
+
+# String constants
+STR_OK = utils.getString(30010)
+STR_ERROR = utils.getString(30159)
+STR_BACKUP = utils.getString(30016)
+STR_RESTORE = utils.getString(30017)
+STR_SETTINGS = utils.getString(30099)
+STR_ADVANCED_EDITOR = utils.getString(30125)
+STR_AUTHORIZE_DROPBOX = utils.getString(30027)
+STR_AUTHORIZE_SUCCESS = utils.getString(30106)
+STR_AUTHORIZE_FAILED = utils.getString(30107)
+STR_NO_REMOTE = utils.getString(30045)
+STR_REMOVE_AUTH_TITLE = utils.getString(30093)
+STR_REMOVE_AUTH_LINE1 = utils.getString(30094)
+STR_REMOVE_AUTH_LINE2 = utils.getString(30095)
+
+# Utility functions
+def authorize_dropbox():
+    """Authorize Dropbox."""
+    authorizer = DropboxAuthorizer()
+    if authorizer.authorize():
+        xbmcgui.Dialog().ok(STR_OK, f'{STR_AUTHORIZE_DROPBOX} {STR_AUTHORIZE_SUCCESS}')
+    else:
+        xbmcgui.Dialog().ok(STR_OK, f'{STR_AUTHORIZE_FAILED} {STR_AUTHORIZE_DROPBOX}')
 
 
-def authorize_cloud(cloudProvider):
-    # dropbox
-    if(cloudProvider == 'dropbox'):
-        authorizer = DropboxAuthorizer()
-
-        if(authorizer.authorize()):
-            xbmcgui.Dialog().ok(utils.getString(30010), '%s %s' % (utils.getString(30027), utils.getString(30106)))
-        else:
-            xbmcgui.Dialog().ok(utils.getString(30010), '%s %s' % (utils.getString(30107), utils.getString(30027)))
+def authorize_cloud(cloud_provider):
+    """Authorize the specified cloud provider."""
+    if cloud_provider == 'dropbox':
+        authorize_dropbox()
 
 
 def remove_auth():
-    # triggered from settings.xml - asks if user wants to delete OAuth token information
-    shouldDelete = xbmcgui.Dialog().yesno(utils.getString(30093), '%s\n%s' % (utils.getString(30094), utils.getString(30095)), autoclose=7000)
+    """Remove authorization for cloud providers."""
+    should_delete = xbmcgui.Dialog().yesno(
+        STR_REMOVE_AUTH_TITLE,
+        f'{STR_REMOVE_AUTH_LINE1}\n{STR_REMOVE_AUTH_LINE2}',
+        autoclose=7000
+    )
 
-    if(shouldDelete):
-        # delete any of the known token file types
-        xbmcvfs.delete(xbmcvfs.translatePath(utils.data_dir() + "tokens.txt"))  # dropbox
-        xbmcvfs.delete(xbmcvfs.translatePath(utils.data_dir() + "google_drive.dat"))  # google drive
-
+    if should_delete:
+        # Delete any of the known token file types
+        xbmcvfs.delete(xbmcvfs.translatePath(utils.data_dir() + "tokens.txt"))  # Dropbox
+        xbmcvfs.delete(xbmcvfs.translatePath(utils.data_dir() + "google_drive.dat"))  # Google Drive
 
 def get_params():
-    param = {}
+    """Extract parameters from the command line arguments."""
     try:
-        for i in sys.argv:
-            args = i
-            if('=' in args):
-                if(args.startswith('?')):
-                    args = args[1:]  # legacy in case of url params
-                splitString = args.split('=')
-                utils.log(splitString[1])
-                param[splitString[0]] = splitString[1]
-    except:
-        pass
+        query = urlparse(sys.argv[1]).query
+        return {k: v[0] for k, v in parse_qs(query).items()}
+    except IndexError:
+        return {}
 
-    return param
+def get_mode(params):
+    """Get the mode based on the provided parameters or from the user."""
+    if "mode" in params:
+        if params['mode'] == 'backup':
+            return Mode.BACKUP
+        elif params['mode'] == 'restore':
+            return Mode.RESTORE
+        elif params['mode'] == 'launcher':
+            return Mode.LAUNCHER
+
+    # If mode wasn't passed in as arg, get from user
+    options = [STR_BACKUP, STR_RESTORE, STR_SETTINGS]
+
+    # Check if we're using the advanced editor
+    if utils.getSettingInt('backup_selection_type') == 1:
+        options.append(STR_ADVANCED_EDITOR)
+
+    # Get the mode from the user
+    selected_mode = xbmcgui.Dialog().select(STR_OK, options)
+    return Mode(selected_mode) if selected_mode != -1 else None
 
 
-# the program mode
-mode = -1
-params = get_params()
+def handle_no_remote_configured():
+    """Handle the case when remote is not configured."""
+    xbmcgui.Dialog().ok(STR_OK, STR_NO_REMOTE)
+    utils.openSettings()
 
-if("mode" in params):
-    if(params['mode'] == 'backup'):
-        mode = BACKUP
-    elif(params['mode'] == 'restore'):
-        mode = RESTORE
-    elif(params['mode'] == 'launcher'):
-        mode = LAUNCHER
 
-# if mode wasn't passed in as arg, get from user
-if(mode == -1):
-    # by default, Backup,Restore,Open Settings
-    options = [utils.getString(30016), utils.getString(30017), utils.getString(30099)]
+def handle_backup():
+    """Handle the backup operation."""
+    backup = XbmcBackup()
 
-    # find out if we're using the advanced editor
-    if(utils.getSettingInt('backup_selection_type') == 1):
-        options.append(utils.getString(30125))
+    if not backup.remoteConfigured():
+        handle_no_remote_configured()
+        return
 
-    # figure out if this is a backup or a restore from the user
-    mode = xbmcgui.Dialog().select(utils.getString(30010) + " - " + utils.getString(30023), options)
+    backup.backup()
 
-# check which mode should be run
-if(mode != -1):
+def handle_restore(params):
+    """Handle the restore operation."""
+    backup = XbmcBackup()
 
-    if(mode == SETTINGS):
-        # open the settings dialog
-        utils.openSettings()
-    elif(mode == ADVANCED_EDITOR and utils.getSettingInt('backup_selection_type') == 1):
-        # open the advanced editor but only if in advanced mode
+    if not backup.remoteConfigured():
+        handle_no_remote_configured()
+        return
+
+    restore_points = backup.listBackups()
+    point_names = []
+    folder_names = []
+
+    for a_dir in restore_points:
+        point_names.append(a_dir[1])
+        folder_names.append(a_dir[0])
+
+    selected_restore = -1
+
+    if "archive" in params:
+        if params['archive'] in folder_names:
+            selected_restore = folder_names.index(params['archive'])
+            utils.log(f'{selected_restore} : {params["archive"]}')
+        else:
+            utils.showNotification(STR_NO_REMOTE)
+            utils.log(f'{params["archive"]} is not a valid restore point')
+    else:
+        selected_restore = xbmcgui.Dialog().select(STR_OK, point_names)
+
+    if selected_restore != -1:
+        backup.selectRestore(restore_points[selected_restore][0])
+
+    if 'sets' in params:
+        backup.restore(selectedSets=params['sets'].split('|'))
+    else:
+        backup.restore()
+
+
+def handle_settings():
+    """Handle the settings operation."""
+    utils.openSettings()
+
+
+def handle_advanced_editor():
+    """Handle the advanced editor operation."""
+    if utils.getSettingInt('backup_selection_type') == 1:
         editor = AdvancedBackupEditor()
         editor.showMainScreen()
-    elif(mode == LAUNCHER):
-        # copied from old launcher.py
-        if(params['action'] == 'authorize_cloud'):
-            authorize_cloud(params['provider'])
-        elif(params['action'] == 'remove_auth'):
-            remove_auth()
-        elif(params['action'] == 'advanced_editor'):
-            editor = AdvancedBackupEditor()
-            editor.showMainScreen()
-        elif(params['action'] == 'advanced_copy_config'):
-            editor = AdvancedBackupEditor()
-            editor.copySimpleConfig()
 
-    elif(mode == BACKUP or mode == RESTORE):
-        backup = XbmcBackup()
 
-        # if mode was RESTORE
-        if(mode == RESTORE and backup.remoteConfigured()):
-            # get list of valid restore points
-            restorePoints = backup.listBackups()
-            pointNames = []
-            folderNames = []
+def handle_launcher(params):
+    """Handle the launcher operation."""
+    launcher_actions = {
+        'authorize_cloud': lambda: authorize_cloud(params['provider']),
+        'remove_auth': remove_auth,
+        'advanced_editor': handle_advanced_editor,
+        'advanced_copy_config': AdvancedBackupEditor().copySimpleConfig
+    }
 
-            for aDir in restorePoints:
-                pointNames.append(aDir[1])
-                folderNames.append(aDir[0])
-
-            selectedRestore = -1
-
-            if("archive" in params):
-                # check that the user give archive exists
-                if(params['archive'] in folderNames):
-                    # set the index
-                    selectedRestore = folderNames.index(params['archive'])
-                    utils.log(str(selectedRestore) + " : " + params['archive'])
-                else:
-                    utils.showNotification(utils.getString(30045))
-                    utils.log(params['archive'] + ' is not a valid restore point')
-            else:
-                # allow user to select the backup to restore from
-                selectedRestore = xbmcgui.Dialog().select(utils.getString(30010) + " - " + utils.getString(30021), pointNames)
-
-            if(selectedRestore != -1):
-                backup.selectRestore(restorePoints[selectedRestore][0])
-
-            if('sets' in params):
-                backup.restore(selectedSets=params['sets'].split('|'))
-            else:
-                backup.restore()
-        elif(mode == BACKUP and backup.remoteConfigured()):
-            # mode was BACKUP
-            backup.backup()
-        else:
-            # can't go any further
-            xbmcgui.Dialog().ok(utils.getString(30010), utils.getString(30045))
-            utils.openSettings()
+    action = params['action']
+    if action in launcher_actions:
+        launcher_actions[action]()
     else:
-        xbmcgui.Dialog().ok(utils.getString(30010), "%s %s" % (utils.getString(30159), params['mode']))
+        xbmcgui.Dialog().ok(STR_OK, f'{STR_ERROR} {params["action"]}')
+
+
+def main():
+    """Main function to handle the operations."""
+    try:
+        mode_functions = {
+            Mode.BACKUP: handle_backup,
+            Mode.RESTORE: handle_restore,
+            Mode.SETTINGS: handle_settings,
+            Mode.ADVANCED_EDITOR: handle_advanced_editor,
+            Mode.LAUNCHER: handle_launcher,
+        }
+
+        params = get_params()
+        mode = get_mode(params)
+        if mode in mode_functions:
+            mode_functions[mode](params)
+        else:
+            xbmcgui.Dialog().ok(STR_OK, f'{STR_ERROR} {mode}')
+    except Exception as e:
+        utils.log(f'Error: {e}', loglevel=xbmc.LOGERROR)
+        xbmcgui.Dialog().ok(STR_OK, f'{STR_ERROR} {e}')
+
+
+if __name__ == "__main__":
+    main()
